00158572c48bcd00437c0628f78188bf12ce051b
Sumit Rahman <sumitrahman@yahoo.com>
2016-01-27 00:22:32 +0000

    basic functions complete
    The "recommend" function is done, which takes a variable name as input
    and outputs the recommended probabilities for that variable's values
    (and the implied overall probabilities)

--- R/perfman.R
+++ R/perfman.R
@@ -11,19 +11,20 @@ test.data$outcome <- factor(test.data$outcome, levels = c(1,2,3,4))
 #ask for what to do with missing values
 #default is to replace all missing values NA with "NA"
 
+#fit the ordered logistic model
 fit<-polr(data = test.data)
 
-#create function P.X(X, outcome, variable, value) that will be treated
-#as a function of X and give a specific probability for given outcome k for 
-#specific value of a specific variable
-
+#create the cuts as a numeric vector
 cuts<-numeric(length(fit$zeta)+2)
 cuts[2:(length(fit$zeta)+1)]<-fit$zeta
 cuts[c(1,length(cuts))]<-c(-Inf,Inf)
 
+#create a vector of names of explanatory variables
 variables<-attr(fit$terms,"term.labels")
 
 #create the coefficient lookup function
+#a convenient function for reading off the estimated coefficients for all
+#the explanatory variables (including zero for baseline values)
 coef.lookup<-function(variable,value){
     if (!(variable %in% variables)) {
         stop("variable not in model")
@@ -35,11 +36,15 @@ coef.lookup<-function(variable,value){
         return (fit$coefficients[[paste0(variable,value)]])
 }
 
+#create function P.X(X, outcome, variable, value) that will be treated
+#as a function of X and give a specific probability for given outcome k for 
+#specific value of a specific variable
+#what happens with invalid variables/values??
 P.X<-function(X,outcome,variable,value){
     (1+exp(X+coef.lookup(variable,value)-cuts[outcome+1]))^-1-(1+exp(X+coef.lookup(variable,value)-cuts[outcome]))^-1
 }
 
-#get observed proportions of each variable in the data
+#get observed proportions of each variable in the data (including outcome)
 counts.raw<-apply(test.data,2,table)
 proportions.raw<-lapply(counts.raw,FUN = function(x){x/nrow(test.data)})
 
@@ -75,7 +80,27 @@ recommend<-function(variable){
     if (!(variable %in% variables)) {
         stop("variable not in model")
     }
-    optimised<-optimise(f = function(x){squared.error(x,variable)},interval = c(-100,100))
+    optimised<-optimise(f = function(x){squared.error(x,variable)},
+                        interval = c(-100,100))
     message("The recommended value of X is ",optimised$minimum)
 #create data frame with outcomes along the top and variable values down the side
+#    optimised.probs<-matrix(nrow = length(fit$lev),
+ #                           ncol = length(fit$xlevels[[variable]]))
+    
+    optimised.probs<-data.frame(sapply(fit$xlevels[[variable]],
+                                FUN=function(q){
+                                    P.X(X=optimised$minimum,
+                                        outcome = 1:length(fit$lev),
+                                        variable = variable,
+                                        value = q)}))
+    optimised.probs<-cbind(1:length(fit$lev),
+                           sapply(1:length(fit$lev), FUN=function(q){
+                               implied.proportion(X=optimised$minimum,
+                                                  outcome = q,
+                                                  variable = variable)}),
+                           optimised.probs)
+    names(optimised.probs)[1]<-names(fit$model[1])
+    names(optimised.probs)[2]<-"implied probability"
+
+    optimised.probs
 }
